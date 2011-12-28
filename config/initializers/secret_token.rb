1f14d5521e81372545e3434d1b665f4f89a08fa3
Takumi MIURA <mitaku1104@gmail.com>
2011-12-28 19:45:04 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoro::Application.config.secret_token = 'aeb5e52fb29f5d7c7bbb80f2128ef5d46dc29a51b6906f4805085b1504391e3623fbbe9238e929204a268367724a5cb050b83cec9fcf5394c95b031839d3cbc7'
