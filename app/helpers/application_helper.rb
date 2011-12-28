f54d7c2487752e9c088be0c5702657bc03ae5115
Takumi MIURA <mitaku1104@gmail.com>
2011-12-28 21:13:58 +0900

    Write Style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def brand_logo
+    link_to(image_tag("brand.png"), root_path, :class => "brand")
+  end
+
+  def head_tags
+    capture_haml do
+      haml_tag :title, @title || "ToDoRo - ToDo Application"
+      haml_tag :link, :href => "/apple-touch-icon.png", :rel => "apple-touch-icon"
+      haml_tag :meta, :name => "viewport", :content => "width=device-width, initial-scale=1.0, maximum-scale=0.6667, user-scalable=0;" 
+    end
+  end
+
+  def footer_content
+    capture_haml do
+      haml_tag :p, %Q{&copy; Takumi MIURA 2011}.html_safe
+    end
+  end
 end
