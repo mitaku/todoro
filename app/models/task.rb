da457d1f4cc91a22c9c2dd1d2e22cc56304adc59
Takumi MIURA <mitaku1104@gmail.com>
2011-12-30 22:19:20 +0900

    add Task#done

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,9 @@ class Task < ActiveRecord::Base
   attr_accessible :name
 
   validates :name, :presence => true, :length => {:maximum => 140}
+
+  def done!
+    self.done = true
+    self.save!
+  end
 end
